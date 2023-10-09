bea6d72a0e3b145c163690bfd17c6eb064f77903
Istel90 <istel90@gmail.com>
2023-10-09 10:37:40 +0900

    20231009

--- EX_Class.py
+++ EX_Class.py
@@ -23,14 +23,34 @@ LA.norm(dirVec)
 LA.norm(online)
 
 
+#%% one line 으로 데이터 관리하기
+a = np.arange(12)
+# reshape 3 x 4, n X m 행렬로 만들기
+n = a // 4 # n에는 m의 크기에 대한 몫이 저장됨
+m = a % 4 # m에는 m의 크기에 대한 나머지가 저장됨
+print(n)
+print(m)
+matrix = a.reshape((3, 4))
+matrix
+matrix.shape
+
+# 반대의 경우
+c = n * 4 + m
+print(n)
+print(m)
+print(c)
+
 #%%
+oneline = np.arange(12)
+a = oneline.reshape((4, 3))
 
+x = oneline // a.shape[1]
+y = oneline % a.shape[1]
+ToMatrix = x * a.shape[1] + y
 
-
-
-
-
-
+print(x)
+print(y)
+print(ToMatrix)
 
 
 
